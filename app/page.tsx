6f4e519d79268f76b5535b2430fbfb786c809365
Ashish <ashishfun008@gmail.com>
2024-10-08 02:40:18 +0530

    added dp

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ export default function Page() {
     <section>
       <a href={socialLinks.twitter} target="_blank">
         <Image
-          src="/profile.png"
+          src="/IMG_2461.png"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
-          unoptimized
-          width={160}
-          height={160}
+          unoptimized={false}
+          width={180}
+          height={180}
           priority
         />
       </a>
